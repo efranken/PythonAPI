4de24be93ae6e91001e91a816db30b721e2b589f
efranken <efrankie@gmail.com>
2020-11-30 15:09:59 -0500

    Update apiCallAndDynamoWrite.py

--- apiCallAndDynamoWrite.py
+++ apiCallAndDynamoWrite.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import boto3
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb')
 table=dynamodb.Table('pets')
@@ -9,12 +10,33 @@ response = requests.get("https://bzy4xhbr70.execute-api.us-east-2.amazonaws.com/
 
 #convert to json
 parsed = response.json()
+array = []
+
+
 
 print(parsed)
+print(response)
 
-for id in parsed:
-    id = int(parsed['id'])
-    type = parsed['type']
-    price = parsed['price']
-    print("Adding ID:", id, type)
-    table.put_item(Item=parsed)
+for item in parsed:
+    array_items = {"id":None, "type":None, "price":None}
+    array_items['id'] = item['id']
+    array_items['type'] = item['type']
+    array_items['price'] = item['price']
+
+    array.append(array_items)
+
+    table.put_item(
+       Item={
+        'id': str(array_items['id']),
+        'type': array_items['type'],
+        'price': Decimal(str(array_items['price']))
+    })
+
+print(array)
+
+# for i in parsed:
+    #id = int(parsed['id'])
+    #type = parsed['type']
+    #price = parsed['price']
+    #print("Item Number: ", parsed[i])
+    #table.put_item(Item=parsed)
